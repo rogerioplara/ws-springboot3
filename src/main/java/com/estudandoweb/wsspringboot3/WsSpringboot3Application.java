e5dcfe80a6050873f655a9f56be154a80fc4c68c
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2024-02-06 17:03:18 -0300

    Initial commit

---
+++ src/main/java/com/estudandoweb/wsspringboot3/WsSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.estudandoweb.wsspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSpringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSpringboot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudandoweb/wsspringboot3/WsSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudandoweb.wsspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSpringboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
