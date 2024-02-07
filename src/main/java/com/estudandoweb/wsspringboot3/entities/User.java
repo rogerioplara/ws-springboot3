c957f2dc3c440041230081646f76bc358d22daf6
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2024-02-07 17:10:38 -0300

    H2 database, test profile, JPA

--- src/main/java/com/estudandoweb/wsspringboot3/entities/User.java
+++ src/main/java/com/estudandoweb/wsspringboot3/entities/User.java
@@ -1,9 +1,17 @@
 package com.estudandoweb.wsspringboot3.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+// conversão do objeto para o modelo relacional a partir das annotations
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
