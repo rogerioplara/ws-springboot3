67720c6290ea3d9e9542d8e39352d79925f7395e
Rogerio pc Interativa <rogerioo.lara@hotmail.com>
2024-02-07 17:57:53 -0300

    Service layer, component registration

--- src/main/java/com/estudandoweb/wsspringboot3/resources/UserResource.java
+++ src/main/java/com/estudandoweb/wsspringboot3/resources/UserResource.java
@@ -1,18 +1,33 @@
 package com.estudandoweb.wsspringboot3.resources;
 
 import com.estudandoweb.wsspringboot3.entities.User;
+import com.estudandoweb.wsspringboot3.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "99999", "123456");
-        return ResponseEntity.ok().body(u);
+    // dependência para o service
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
---
+++ src/main/java/com/estudandoweb/wsspringboot3/services/UserService.java
@@ -0,0 +1,30 @@
+package com.estudandoweb.wsspringboot3.services;
+
+import com.estudandoweb.wsspringboot3.entities.User;
+import com.estudandoweb.wsspringboot3.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+// @Component registra como um componente do spring para ser injetado com o @Autowired
+// @Service é um registro específico para classes de servico
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        // uso do optional caso não tenha resultado, não gera nullpointer
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
